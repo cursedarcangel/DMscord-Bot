6cbfff5b55b32962056f751464efd23094370f16
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-01-27 18:59:05 -0500

    Moving dice rolling to different file

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const init = require('./initiative.js');
+const init = require('./initiative');
+const dice = require('./roll');
 
 //logging in the bot
 client.login(config.token);
@@ -10,42 +11,9 @@ client.on('ready', loggedIn);
 
 //making sure we are logged in
 function loggedIn() {
-	console.log('Logged in and ready to go');
+	console.log('Logged in');
 }
 
 init.initiative(client);
 
-client.on('message', message => {
-	msg = message.content
-	let name = message.author.username
-
-	//dice roller
-	if (msg.includes('!roll')) {
-		let inpt = msg.split('d');
-		let roll = inpt[1];
-		if (roll.includes('+')) {
-			let die = parseInt(roll.split('+')[0]);
-			let mod = parseInt(roll.split('+')[1]);
-
-			let dieRoll = Math.floor(Math.random() * die) + 1;
-			console.log(dieRoll);
-			dieRoll += mod;
-			console.log(dieRoll);
-			message.channel.send(name + 's roll: ' + dieRoll);
-		}
-		else if (roll.includes('-')) {
-                        let die = parseInt(roll.split('-')[0]);
-                        let mod = parseInt(roll.split('-')[1]);
-                        
-                        let dieRoll = Math.floor(Math.random() * die) + 1;
-                        dieRoll -= mod;
-                        
-                        message.channel.send(name + 's roll: ' + dieRoll);
-                }
-		else {
-			let die = roll;
-			let dieRoll = Math.floor(Math.random() * die) + 1;
-			message.channel.send(name + 's roll: ' + dieRoll);
-		}
-	}
-});
+dice.roll(client);
---
+++ roll.js
@@ -0,0 +1,37 @@
+function roll(client) {
+	client.on('message', message => {
+        msg = message.content
+        let name = message.author.username
+
+        //dice roller
+        if (msg.includes('!roll')) {
+                let inpt = msg.split('d');
+                let roll = inpt[1];
+                if (roll.includes('+')) {
+                        let die = parseInt(roll.split('+')[0]);
+                        let mod = parseInt(roll.split('+')[1]);
+
+                        let dieRoll = Math.floor(Math.random() * die) + 1;
+                        dieRoll += mod;
+                        message.channel.send(name + 's roll: ' + dieRoll);
+                }
+                else if (roll.includes('-')) {
+                        let die = parseInt(roll.split('-')[0]);
+                        let mod = parseInt(roll.split('-')[1]);
+                        
+                        let dieRoll = Math.floor(Math.random() * die) + 1;
+                        dieRoll -= mod;
+                        
+                        message.channel.send(name + 's roll: ' + dieRoll);
+                }
+                else {
+                        let die = roll;
+                        let dieRoll = Math.floor(Math.random() * die) + 1;
+                        message.channel.send(name + 's roll: ' + dieRoll);
+                }
+        }
+});
+
+}
+
+module.exports = { roll };
