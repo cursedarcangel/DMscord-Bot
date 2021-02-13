02eddb56f97b1244f309d6d8665dd078d510afec
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-02-13 13:28:09 -0500

    Changing the way magic item values are input

--- magicItemTracker/magicTrack.js
+++ magicItemTracker/magicTrack.js
@@ -9,9 +9,27 @@ function magicItemTracker(client) {
 		let msg = message.content;
 		if (msg.startsWith('!additem')) {
 			let item = msg.substr(9);
-			item = item.split(' | ');
+			item = item.split(':');
+			for (i = 0; i < item.length; i++) {
+				item[i] = item[i].split(' ');
+			}
+			item[1].unshift(item[0].pop());
+			item[2].unshift(item[1].pop());
+			item[3].unshift(item[2].pop());
+			item.shift();
+			desc = ''
+			for (i = 1; i < item[2].length; i++) {
+				desc += item[2][i];
+				desc += ' ';
+			}
+			desc = desc.substr(0, desc.length - 1);
+			let descr = item[2][0];
+			item.pop();
+			let arr = [descr, desc];
+			item.push(arr);
+			console.log(item);
 			itemDict = {};
-			itemDict[item[0].toLowerCase()] = item;
+			itemDict[item[0][1].toLowerCase()] = item;
 			let ymlItem = yaml.dump(itemDict);
 			itemDict = ''
 			fs.appendFile('./magicItemTracker/items.yml', ymlItem, (err) => {if (err) {message.channel.send('Sorry, couldnt add that item. There is probably another item with that name. Try giving the item a different name.')}});
