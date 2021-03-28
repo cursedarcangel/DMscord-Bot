bbd8e6a541900b003492472debc92db6f3769bb6
Bradford Powell <bpow@unc.edu>
2021-03-28 12:54:18 -0400

    updates to account for rolling multiple dice with modifier, like 2d4+2
    In the process, also:
    - splits out process_roll to enhance testability
    - allows multiple rolls per line
    - sends a message (and to console.log) if there is an error parsing
    - uses regexp instead of manual splitting
    
    relevant to #1

--- diceRoller/roll.js
+++ diceRoller/roll.js
@@ -1,37 +1,31 @@
+die_regexp = /^(?<die_count>\d+)?d(?<die_size>\d+)(?<mod>[+-]\d+)?$/
+
 function roll(client) {
 	client.on('message', message => {
-        msg = message.content
-        let name = message.author.username
+                msg = message.content
+                let name = message.author.username
 
-        //dice roller
-        if (msg.startsWith('!roll')) {
-                let inpt = msg.split('d');
-                let roll = inpt[1];
-                if (roll.includes('+')) {
-                        let die = parseInt(roll.split('+')[0]);
-                        let mod = parseInt(roll.split('+')[1]);
+                // dice roller
+                if (msg.startsWith('!roll')) {
+                        msg.split(/\s+/).slice(1).forEach((roll_string) => {
+                                process_roll(roll_string, name, message.channel.send)
+                        })
+                }
+        });
 
-                        let dieRoll = Math.floor(Math.random() * die) + 1;
-                        dieRoll += mod;
-                        message.channel.send(name + 's roll: ' + dieRoll);
-                }
-                else if (roll.includes('-')) {
-                        let die = parseInt(roll.split('-')[0]);
-                        let mod = parseInt(roll.split('-')[1]);
-                        
-                        let dieRoll = Math.floor(Math.random() * die) + 1;
-                        dieRoll -= mod;
-                        
-                        message.channel.send(name + 's roll: ' + dieRoll);
-                }
-                else {
-                        let die = roll;
-                        let dieRoll = Math.floor(Math.random() * die) + 1;
-                        message.channel.send(name + 's roll: ' + dieRoll);
-                }
+}
+
+function process_roll(roll_string, user = 'test_user', msg_callback = console.log) {
+        let match
+        if (match = die_regexp.exec(roll_string)) {
+                let die_count = +match.groups.die_count || 1;
+                let mod = +match.groups.mod || 0;
+                let rolls = [...Array(die_count)].map(() => Math.floor(Math.random() * match.groups.die_size) + 1)
+                msg_callback(`${user}'s roll: [${rolls}] + ${mod} = ${rolls.reduce((a,b) => a+b, mod)}`)
+        } else {
+                console.log(`ERROR in roll: ${roll_string}`)
+                msg_callback(`ERROR: ${user} tried to roll with '${roll_string}, which made no sense`)
         }
-});
-
 }
 
 module.exports = { roll };
