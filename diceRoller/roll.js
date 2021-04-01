a4cc6ce15b172b69caeb162bd9003bb95384c70d
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-04-01 12:29:54 -0400

    Adding rolling with initiative

--- diceRoller/roll.js
+++ diceRoller/roll.js
@@ -1,14 +1,14 @@
-die_regexp = /^(?<die_count>\d+)?d(?<die_size>\d+)(?<mod>[+-]\d+)?$/
+die_regexp = /^(?<die_count>\d+)?d(?<die_size>\d+)(?<mod>[+-]\d+)?(?<advantage>[<>])?$/
 
 function roll(client) {
 	client.on('message', message => {
-                msg = message.content
-                let name = message.author.username
+                msg = message.content;
+                let name = message.author.username;
 
                 // dice roller
                 if (msg.startsWith('!roll')) {
                         msg.split(/\s+/).slice(1).forEach((roll_string) => {
-                                process_roll(roll_string, name, (res) => message.channel.send(res))
+                                process_roll(roll_string, name, (res) => message.channel.send(res));
                         })
                 }
         });
@@ -16,15 +16,30 @@ function roll(client) {
 }
 
 function process_roll(roll_string, user = 'test_user', msg_callback = console.log) {
-        let match
+        let match;
         if (match = die_regexp.exec(roll_string)) {
                 let die_count = +match.groups.die_count || 1;
+		let advantage = match.groups.advantage;
                 let mod = +match.groups.mod || 0;
-                let rolls = [...Array(die_count)].map(() => Math.floor(Math.random() * match.groups.die_size) + 1)
-                msg_callback(`${user}'s roll: [${rolls}] + ${mod} = ${rolls.reduce((a,b) => a+b, mod)}`)
+                let rolls = [...Array(die_count)].map(() => Math.floor(Math.random() * match.groups.die_size) + 1);
+		if (advantage) {
+			let selected;
+			let sum1 = rolls.reduce((a,b) => a+b, mod);
+                	let roll2 = [...Array(die_count)].map(() => Math.floor(Math.random() * match.groups.die_size) + 1);
+			let sum2 = roll2.reduce((a,b) => a+b, mod);
+			if (advantage == '<') {
+				selected = Math.min(sum1, sum2);
+			} else {
+				selected = Math.max(sum1, sum2);
+			}
+			msg_callback(`${user}'s rolls: ${rolls} + ${mod} = ${sum1}  |  ${roll2} + ${mod} = ${sum2}  |  -> ${selected}`);
+
+		} else {
+			msg_callback(`${user}'s roll: ${rolls} + ${mod} = ${rolls.reduce((a,b) => a+b, mod)}`);
+		}
         } else {
                 console.log(`ERROR in roll: ${roll_string}`)
-                msg_callback(`ERROR: ${user} tried to roll with '${roll_string}, which made no sense`)
+                msg_callback(`ERROR: ${user} tried to roll with '${roll_string}, which made no sense`);
         }
 }
 
